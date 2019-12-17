90c86169d353f4b6e4d52b3801672a863ed91c48
Andrew Kelley <kelley.andrew.t@gmail.com>
2019-12-17 13:22:30 -0500

    First commit

---
+++ mySpaceStuff/views.py
@@ -0,0 +1,26 @@
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView
+from django.shortcuts import render
+import requests
+
+class TestPage(TemplateView):
+    template_name = 'test.html'
+
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
+
+class HomePage(TemplateView):
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        response = requests.get('https://api.nasa.gov/planetary/apod?api_key=suY5NhcHycX1CIkDaMCXNdY8dIYdp0O5meo3cJso')
+        daily_image_data = response.json()
+        kwargs['date'] = daily_image_data['date']
+        kwargs['title'] = daily_image_data['title']
+        kwargs['description'] = daily_image_data['explanation']
+        kwargs['image_url'] = daily_image_data['hdurl']
+
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse("test"))
+        return super().get(request, *args, **kwargs)
