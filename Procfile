900e3d5118b1c178560245ca64fed003643badd1
Andrew Kelley <kelley.andrew.t@gmail.com>
2019-12-18 10:17:09 -0500

    changes to settings.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn locallibrary.wsgi --log-file -
+web: gunicorn mySpaceStuff.wsgi --log-file -
--- mySpaceStuff/settings.py
+++ mySpaceStuff/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '@4g7@if44(nmy*m@vju$y#j(ymkmq@p0k9_)vx+ino3ei$r+09'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '@4g7@if44(nmy*m@vju$y#j(ymkmq@p0k9_)vx+ino3ei$r+09')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
