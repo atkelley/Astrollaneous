7390fd3de503d188cdfa3e5b74af32cebab88af3
Andrew Kelley <kelley.andrew.t@gmail.com>
2019-12-18 10:23:05 -0500

    updated ALLOWED_HOSTS in settings.py

--- mySpaceStuff/settings.py
+++ mySpaceStuff/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '@4g7@if44(nmy*m@vju$y#j(ymkmq@
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['starstuffer.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
