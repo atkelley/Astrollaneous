2f4ca3db81f5bb40424aae391c21285a9c7d3bb9
Andrew Kelley <kelley.andrew.t@gmail.com>
2020-06-04 16:21:50 -0400

    NASA page working; checkboxes still remaining

--- static/mySpaceStuff/js/master.js
+++ static/mySpaceStuff/js/master.js
@@ -9,12 +9,14 @@ function nextSlide(n) {
 function showSlide(n) {
   var slides = document.getElementsByClassName("nasa-slides");
 
-  if (n < 1) { slideIndex = slides.length; }
-  if (n > slides.length) { slideIndex = 1; }
-
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  if (slides.length != 0) {
+    if (n < 1) { slideIndex = slides.length; }
+    if (n > slides.length) { slideIndex = 1; }
+  
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+  
+    slides[slideIndex - 1].style.display = "block";
   }
-
-  slides[slideIndex - 1].style.display = "block";
 } 
