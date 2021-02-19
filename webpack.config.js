78786f17a54bcb870ada667b214473e26f1ad959
Andrew Kelley <kelley.andrew.t@gmail.com>
2021-02-19 16:08:36 -0500

    Different logic for listing variables from .env

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,35 @@ const Dotenv = require('dotenv-webpack');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
 
-module.exports = { 
-  watch: false,
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-          loader: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        use: [ "style-loader", "css-loader" ]
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loader: "url-loader"
-      }
+module.exports = () => { 
+  const env = dotenv.config().parsed;
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    watch: false,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+            loader: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          use: [ "style-loader", "css-loader" ]
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          loader: "url-loader"
+        }
+      ]
+    },
+    plugins: [
+      new Dotenv(),
+      new webpack.DefinePlugin(envKeys)
     ]
-  },
-  plugins: [
-    new Dotenv(),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(dotenv.config().parsed) 
-   })
-  ]
+  }
 };
