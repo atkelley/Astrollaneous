23421c0be839b922ecc9479244462be01f8e3a5a
Andrew Kelley <kelley.andrew.t@gmail.com>
2021-02-19 15:07:59 -0500

    Using dotenv-webpack python package to define .env

--- frontend/src/components/apis/api.js
+++ frontend/src/components/apis/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const baseURL = (process.env && process.env.NODE_ENV && process.env.NODE_ENV == 'development') ? 'http://localhost:8000/api' : 'http://astrollaneous.herokuapp.com';
+const baseURL = (process.env.NODE_ENV == 'development') ? 'http://localhost:8000/api' : 'http://astrollaneous.herokuapp.com';
 
 export default axios.create({ baseURL: baseURL, headers: { "Content-type": "application/json" } });
