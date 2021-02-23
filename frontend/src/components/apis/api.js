34a0f40df3066d240e0129c21f14f9d899d7bdbf
Andrew Kelley <kelley.andrew.t@gmail.com>
2021-02-23 16:11:49 -0500

    Updated DB settings

--- frontend/src/components/apis/api.js
+++ frontend/src/components/apis/api.js
@@ -1,5 +1,3 @@
 import axios from "axios";
 
-const baseURL = (process.env.NODE_ENV) ? 'http://localhost:8000/api' : 'http://astrollaneous.herokuapp.com';
-
-export default axios.create({ baseURL: baseURL, headers: { "Content-type": "application/json" } });
+export default axios.create({ baseURL: 'http://astrollaneous.herokuapp.com', headers: { "Content-type": "application/json" } });
