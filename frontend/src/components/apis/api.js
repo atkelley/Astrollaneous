38715cdf18fdd97a92b52f4e5ba7dadc691180a6
Andrew Kelley <kelley.andrew.t@gmail.com>
2021-02-23 21:50:48 -0500

    Switching to HTTPS protocol for API endpoint(s)

--- frontend/src/components/apis/api.js
+++ frontend/src/components/apis/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const baseURL = (process.env.NODE_ENV == 'development') ? 'http://localhost:8000/api' : 'http://astrollaneous.herokuapp.com/api';
+const baseURL = (process.env.NODE_ENV == 'development') ? 'https://localhost:8000/api' : 'https://astrollaneous.herokuapp.com/api';
 
 export default axios.create({ baseURL: baseURL, headers: { "Content-type": "application/json" } });
