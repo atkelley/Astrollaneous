171d1b6c53e6b78b9a6adfaad98f2405544e520c
Andrew Kelley <kelley.andrew.t@gmail.com>
2021-02-23 21:42:45 -0500

    Re-creating condition for api baseURL

--- frontend/src/components/apis/api.js
+++ frontend/src/components/apis/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const baseURL = 'https://astrollaneous.herokuapp.com';
+const baseURL = (process.env.NODE_ENV == 'development') ? 'http://localhost:8000/api' : 'http://astrollaneous.herokuapp.com/api';
 
 export default axios.create({ baseURL: baseURL, headers: { "Content-type": "application/json" } });
