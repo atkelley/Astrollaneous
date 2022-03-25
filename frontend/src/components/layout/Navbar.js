e4b1d5f6003bcccf0014a4e53e7bbc267a98f54e
Andrew T. Kelley <24995895+atkelley@users.noreply.github.com>
2022-03-25 16:16:10 -0400

    Update Navbar.js

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -3,16 +3,28 @@ import { Link } from 'react-router-dom';
 import '../css/layout/Navbar.css';
 
 const Navbar = () => {
+  const handleTabChange = (event) => {
+    $('.nav-tabs .nav-item .nav-link').each(function() { 
+      if($(this).is(`#${event.target.id}`)) {
+        if(!$(this).is('.active')) {
+          $(this).toggleClass(' active');
+        }
+      } else {
+        $(this).removeClass(' active');
+      }
+    });  
+  }
+
   return (
     <div className="container navbar-links">
       <ul className="nav nav-tabs justify-content-center">
-        <li className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
-        <li className="nav-item"><Link to="/mars" className="nav-link">Mars</Link></li>
-        <li className="nav-item"><Link to="/rovers" className="nav-link">Rovers</Link></li>
-        <li className="nav-item"><Link to="/satellites" className="nav-link">Satellites</Link></li>
-        <li className="nav-item"><Link to="/nasa" className="nav-link">NASA</Link></li>
-        <li className="nav-item"><Link to="/techport" className="nav-link">TechPort</Link></li>
-        <li className="nav-item"><Link to="/about" className="nav-link">About</Link></li>
+        <li className="nav-item"><Link to="/" className="nav-link active" id="home" onClick={(event) => handleTabChange(event)}>Home</Link></li>
+        <li className="nav-item"><Link to="/mars" className="nav-link" id="mars" onClick={(event) => handleTabChange(event)}>Mars</Link></li>
+        <li className="nav-item"><Link to="/rovers" className="nav-link" id="rovers" onClick={(event) => handleTabChange(event)}>Rovers</Link></li>
+        <li className="nav-item"><Link to="/satellites" className="nav-link" id="satellites" onClick={(event) => handleTabChange(event)}>Satellites</Link></li>
+        <li className="nav-item"><Link to="/nasa" className="nav-link" id="nasa" onClick={(event) => handleTabChange(event)}>NASA</Link></li>
+        <li className="nav-item"><Link to="/techport" className="nav-link" id="techport" onClick={(event) => handleTabChange(event)}>TechPort</Link></li>
+        <li className="nav-item"><Link to="/about" className="nav-link" id="about" onClick={(event) => handleTabChange(event)}>About</Link></li>
       </ul>
   </div>
   );
