28851a9cf8b1fe6c4eda4d553669c5ff05fb58e8
Andrew Kelley <kelley.andrew.t@gmail.com>
2022-03-28 13:59:54 -0400

    Techport grid cleanup

--- mySpaceStuff/settings.py
+++ mySpaceStuff/settings.py
@@ -89,15 +89,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mySpaceStuff.wsgi.application'
 
 
-# DATABASES = {
-#   'default': {
-#     'ENGINE': 'django.db.backends.sqlite3',
-#     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#   }
-# }
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  }
+}
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+# DATABASES = {}
+# DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 
 
 # Password validation
