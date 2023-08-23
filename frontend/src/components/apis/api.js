5153d29685c39cf06aa8ad1c3ab41aa074c5b19d
Andrew Kelley <kelley.andrew.t@gmail.com>
2023-08-23 16:24:32 -0400

    Changed API logic

--- frontend/src/components/apis/api.js
+++ frontend/src/components/apis/api.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
-const baseURL = (process.env.NODE_ENV == 'development') ? 'http://localhost:8000/api' : 'https://astrollaneous.herokuapp.com/api';
+// const baseURL = (process.env.NODE_ENV == 'development') ? 'http://localhost:8000/api' : 'https://astrollaneous.herokuapp.com/api';
+
+let baseURL = '';
+
+if (window.location.origin === "http://localhost:3000") {
+  baseURL = "http://127.0.0.1:8000"; // development address
+} else {
+  baseURL = window.location.origin; // production address
+}
 
 export default axios.create({ baseURL: baseURL, headers: { "Content-type": "application/json" } });
