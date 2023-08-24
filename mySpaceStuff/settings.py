6c1a17bcebbc2cca554a32126862dc9f58e47747
Andrew Kelley <kelley.andrew.t@gmail.com>
2023-08-24 10:00:07 -0400

    Small settings.py change

--- mySpaceStuff/settings.py
+++ mySpaceStuff/settings.py
@@ -28,10 +28,10 @@ else:
   SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 ALLOWED_HOSTS = ["*"] 
 
 INSTALLED_APPS = [
