b643e1582377f3f08609e916073a00d7ea16bbbc
Andrew Kelley <kelley.andrew.t@gmail.com>
2023-08-25 14:17:31 -0400

    Adjusted static location

--- mySpaceStuff/settings.py
+++ mySpaceStuff/settings.py
@@ -154,8 +154,8 @@ LOGGING = {
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_DIRS = [] 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
